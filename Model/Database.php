a9941c8f44bfa43480334edfb3d98c82e3bbdd74
HengMichel <cavacmoi@gmail.com>
2024-04-23 21:02:43 +0200

    change data

--- Model/Database.php
+++ Model/Database.php
@@ -6,7 +6,7 @@ class Database
 {
     // connexion à la base de données
     private $host = "localhost";
-    private $db_name = "gite_db_v2";
+    private $db_name = "gite_db";
     private $username = "root";
     private $password = "";
     private $connection = null;
